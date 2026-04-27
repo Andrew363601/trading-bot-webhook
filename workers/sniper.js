d1d82d48d6acca79a2df4350c97e7c64de8e2285
Andrew363601 <andrew.siller.as@gmail.com>
2026-04-26 22:34:17 -0500

    added right trade-oracle to sniper.js

--- workers/sniper.js
+++ workers/sniper.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 import jwt from 'jsonwebtoken';
 import crypto from 'crypto';
 import { evaluateStrategy } from '../lib/strategy-router.js';
-import { evaluateTradeIdea } from '../lib/oracle-brain.js';
+import { evaluateTradeIdea } from '../lib/trade-oracle.js';
 import { buildRadarChartUrl } from '../lib/discord-chart.js';
 
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
