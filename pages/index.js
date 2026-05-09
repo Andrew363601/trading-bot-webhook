005b347df1f97ee9c2c100774d4231fdd78c5e81
Andrew <andrew.siller.as@gmail.com>
2026-05-09 09:29:27 -0500

    resolved eslint error

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import {
   Send, Activity, PieChart, Shield, Zap, TrendingUp, TrendingDown,
   Target, AlertTriangle, ArrowRight, RefreshCw, Layers, BrainCircuit,
   Settings, LogOut, Clock, Crosshair, ChevronRight, Menu, X, PlusCircle,
-  Search, AlertOctagon, Eye, Minimize2, Maximize2, Flame, Power
+  Search, AlertOctagon, Eye, Minimize2, Maximize2, Flame, Power, ChevronDown
 } from 'lucide-react';
 import AuthGuard from '../components/AuthGuard';
 import MarketScanner from '../components/MarketScanner';
