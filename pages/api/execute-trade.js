e5ce4b44fc3188465aea954adb26dd7c2071f36c
Andrew <andrew.siller.as@gmail.com>
2026-05-09 20:26:56 -0500

    ntax error in execute-trade.js

--- pages/api/execute-trade.js
+++ pages/api/execute-trade.js
@@ -49,8 +49,6 @@ const getAssetMetrics = (symbol) => {
     return { multiplier, tickSize };
 };
 
-...existing code...
-
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method Not Allowed' });
 
